ab6015f06551eeba1c9cfaa6df7e37b49c616360
isaac-armstrong <77643965+isaac-armstrong@users.noreply.github.com>
2021-02-26 17:08:34 -0500

    Update App.java
    Removed sample template remnants

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,3 @@
-/* Copyright 2020, Gurobi Optimization, LLC */
-
-/* This example formulates and solves the following simple MIP model:
-
-     maximize    x +   y + 2 z
-     subject to  x + 2 y + 3 z <= 4
-                 x +   y       >= 1
-                 x, y, z binary
-*/
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonParseException;
